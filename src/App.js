781e07e4f2c0e7c7b54bef587f1c701998619436
Daniel Tashobya <danieltasho11@gmail.com>
2024-07-14 21:19:16 +0300

    body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import Body from './components/body';
 import './styles/header.css';
 
 const App = () => {
   return (
     <div id='app' className='App'>
-    <Header/>
+    <Header />
+    <Body />
     </div>
     
   );
