3b38aec1fdc301c25944720a5df524c19c4e16d1
Daniel Tashobya <danieltasho11@gmail.com>
2024-07-17 08:33:02 +0300

    ui update

--- src/Routes/districtpage/district.js
+++ src/Routes/districtpage/district.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import  './district.css';
 import nurse from '../../images/nurse.jpg';
+import doc1 from '../../images/doc1.jpg'
+import doc7 from '../../images/doc7.jpg'
+import doc8 from '../../images/doc8.jpeg'
 import { Link } from 'react-router-dom';
-//import Book from '../bookpage/book';
-//import { Link } from 'react-router-dom';
+
 
 
 const District = () => {
@@ -12,10 +14,54 @@ const District = () => {
 <div className="specialists">
 <div className="grid-container">
     <div className="card">
+        <img src={doc1} alt="nurse" style={{width: '100%'}}/>
+        <div className="container">
+            <h3><b>Dr. Millicent Jane</b></h3> 
+            <p>Aga Khan Hospital</p> 
+            <p> Dermatologist</p> 
+            <p>Working Hours : 9:00am to 3:00pm</p>
+            <p>Available  Monday to Friday</p>
+            <Link to='/book'><button className="cardbtn">Book</button></Link>
+        </div>
+    </div>
+    <div className="card">
         <img src={nurse} alt="nurse" style={{width: '100%'}}/>
         <div className="container">
             <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
+            <p>The Union Hospital</p> 
+            <p> Physical Therapist</p> 
+            <p>Working Hours : 9:00am to 3:00pm</p>
+            <p>Available  Monday to Friday</p>
+            <Link to='/book'><button className="cardbtn">Book</button></Link>
+        </div>
+    </div>
+    <div className="card">
+        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
+        <div className="container">
+            <h3><b>Dr. Millicent Jane</b></h3> 
+            <p>District International Hospital</p> 
+            <p> Physician</p> 
+            <p>Working Hours : 9:00am to 3:00pm</p>
+            <p>Available  Monday to Friday</p>
+            <Link to='/book'><button className="cardbtn">Book</button></Link>
+        </div>
+    </div>
+    <div className="card">
+        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
+        <div className="container">
+            <h3><b>Dr. Millicent Jane</b></h3> 
+            <p>St.Peter's Hospital</p> 
+            <p> Genetic counselor</p> 
+            <p>Working Hours : 9:00am to 3:00pm</p>
+            <p>Available  Monday to Friday</p>
+            <Link to='/book'><button className="cardbtn">Book</button></Link>
+        </div>
+    </div>
+    <div className="card">
+        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
+        <div className="container">
+            <h3><b>Dr. Millicent Jane</b></h3> 
+            <p>Johns Hopkins Medical Center</p> 
             <p> Obstetrician-gynecologist</p> 
             <p>Working Hours : 9:00am to 3:00pm</p>
             <p>Available  Monday to Friday</p>
@@ -26,73 +72,29 @@ const District = () => {
         <img src={nurse} alt="nurse" style={{width: '100%'}}/>
         <div className="container">
             <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
-            <p>Working Hours : 9:00am to 3:00pm</p>
-            <p>Available  Monday to Friday</p>
-            <Link to='/book'><button className="cardbtn">Book</button></Link>
-        </div>
-    </div>
-    <div className="card">
-        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
-        <div className="container">
-            <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
-            <p>Working Hours : 9:00am to 3:00pm</p>
-            <p>Available  Monday to Friday</p>
-            <Link to='/book'><button className="cardbtn">Book</button></Link>
-        </div>
-    </div>
-    <div className="card">
-        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
-        <div className="container">
-            <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
-            <p>Working Hours : 9:00am to 3:00pm</p>
-            <p>Available  Monday to Friday</p>
-            <Link to='/book'><button className="cardbtn">Book</button></Link>
-        </div>
-    </div>
-    <div className="card">
-        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
-        <div className="container">
-            <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
-            <p>Working Hours : 9:00am to 3:00pm</p>
-            <p>Available  Monday to Friday</p>
-            <Link to='/book'><button className="cardbtn">Book</button></Link>
-        </div>
-    </div>
-    <div className="card">
-        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
-        <div className="container">
-            <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
+            <p>Metropolitan Hospital</p> 
+            <p> Dietitian</p> 
             <p>Working Hours : 9:00am to 3:00pm</p>
             <p>Available  Monday to Friday</p>
             <Link to='/book'><button className="cardbtn">Book</button></Link>
         </div> 
     </div>
     <div className="card">
-        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
+        <img src={doc7} alt="nurse" style={{width: '100%'}}/>
         <div className="container">
             <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
+            <p>The County Clinic</p> 
+            <p> Audiologist</p> 
             <p>Working Hours : 9:00am to 3:00pm</p>
             <p>Available  Monday to Friday</p>
             <Link to='/book'><button className="cardbtn">Book</button></Link>
         </div>
     </div><div className="card">
-        <img src={nurse} alt="nurse" style={{width: '100%'}}/>
+        <img src={doc8} alt="nurse" style={{width: '100%'}}/>
         <div className="container">
             <h3><b>Dr. Millicent Jane</b></h3> 
-            <p>The Nairobi Hospital</p> 
-            <p> Obstetrician-gynecologist</p> 
+            <p>The Karen Hospital</p> 
+            <p> Occupational Therapist</p> 
             <p>Working Hours : 9:00am to 3:00pm</p>
             <p>Available  Monday to Friday</p>
             <Link to='/book'><button className="cardbtn">Book</button></Link>
