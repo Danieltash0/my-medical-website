34cad454a9f48acb9b1c0a72eebf9d05b8bd3521
Daniel Tashobya <danieltasho11@gmail.com>
2024-07-18 18:07:17 +0300

    socials

--- src/Routes/footer.js
+++ src/Routes/footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './footer.css';
-
+import { Link } from 'react-router-dom';
+import whatsapp from '../images/whatsapp.jpg';
+import xlogo from '../images/xlogo.jpg'
+import gmail from '../images/gmail.jpg'
+import pinterest from '../images/pinterest.png'
 
 const Footer = () => {
   return (
     <footer>
+      <div className='footer-text'>
       <p>Bridging the gap to better healthcare</p>
       <p> Cure Connect @ 2024</p>
-
+      </div>
+    <div className='image-container'>
+    <Link to='/'><div className='social-icon'><img src={whatsapp} alt='socials'/></div></Link>
+    <Link to='/'><div className='social-icon'><img src={xlogo} alt='socials'/></div></Link>
+    <Link to='/'><div className='social-icon'><img src={pinterest} alt='socials'/></div></Link>
+    <Link to='/'><div className='social-icon'><img src={gmail}alt='socials' /></div></Link>
+    </div>
     </footer>
   );
 };
